0541a2701bfe54f6e6cf5b6ccd56c7a1df787c84
Kali <kali@kali.local>
2020-12-12 20:22:08 -0300

    resolve default export

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,36 +1,36 @@
-import genderByName from './index';
+import { getGenderByName } from './index';
 
 beforeAll(() => {
   jest.setTimeout(30000);
 })
 
 it('should return Female when name is Rebeca', async () => {
-  const gender = await genderByName('Rebeca');
+  const gender = await getGenderByName('Rebeca');
   expect(gender).toBe('Female');
 });
 
 it('should return Male when name is Thiago', async () => {
-  const gender = await genderByName('Thiago');
+  const gender = await getGenderByName('Thiago');
   expect(gender).toBe('Male');
 });
 
 
 it('should return NA when name is pretty hard to define by census', async () => {
-  const gender = await genderByName('asdfg');
+  const gender = await getGenderByName('asdfg');
   expect(gender).toBe('NA');
 });
 
 it('should have property percentage when pass to method -> percentage: true', async () => {
-  const gender = await genderByName('Rebeca', { percentage: true });
+  const gender = await getGenderByName('Rebeca', { percentage: true });
   expect(gender).toHaveProperty('percentage');
 });
 
 it('should have property percentage when pass to method -> percentage: true', async () => {
-  const gender = await genderByName('Thiago', { percentage: true });
+  const gender = await getGenderByName('Thiago', { percentage: true });
   expect(gender).toHaveProperty('percentage');
 });
 
 it('should return NA when name is pretty hard to define by census with percentage', async () => {
-  const gender = await genderByName('asdfg', { percentage: true });
+  const gender = await getGenderByName('asdfg', { percentage: true });
   expect(gender).toHaveProperty('percentage');
 });
